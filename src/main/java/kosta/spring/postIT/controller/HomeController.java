d2a511e419be154124fc28085b74fe4852752965
LeeWonSeob <leemuo123@gmail.com>
2018-06-18 16:39:00 +0900

    클래스룸 3차커밋

--- src/main/java/kosta/spring/postIT/controller/HomeController.java
+++ src/main/java/kosta/spring/postIT/controller/HomeController.java
@@ -24,73 +24,72 @@ public class HomeController {
 	private MemberService memberService;
 	@Autowired
 	private CourseService courseService;
-	/*@RequestMapping(value = "/", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(HttpSession session) {
-		
-		
-		return "main/mainpage/index";
-		
-//		session.setAttribute("courseCode", "a1");
-//		return "redirect:/cr/notice/selectList";
-	}*/
+		session.setAttribute("courseCode", "a1");
+		return "redirect:/cr/notice/selectList";
+	}
+
 	/**
 	 * main
-	 * */
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public ModelAndView home(MenteeDTO menteeDTO, MentoDTO mentoDTO, CourseDTO courseDTO, MentoReputationDTO mentoReputationDTO) {
-		ModelAndView mv = new ModelAndView();
-		/**
-		 * 누적 확인
-		 * */
-		int MenteeCount = memberService.selectCountMentee(menteeDTO);
-		int MentoCount = memberService.selectCountMento(mentoDTO);
-		int CourseCount = memberService.selectCountCourse(courseDTO);
-		int RepCount = memberService.selectCountReputation(mentoReputationDTO);
-		List<MentoReputationDTO> mentoRepList = courseService.selectReputation();
-		mv.addObject("MenteeCount", MenteeCount);
-		mv.addObject("MentoCount", MentoCount);
-		mv.addObject("CourseCount", CourseCount);
-		mv.addObject("RepCount",RepCount);
-		mv.addObject("mentoRepList", mentoRepList);
-		mv.setViewName("main/mainpage/index");
-		return mv;
-	}
-	
+	 */
+	// @RequestMapping(value="/", method=RequestMethod.GET)
+	// public ModelAndView home(MenteeDTO menteeDTO, MentoDTO mentoDTO, CourseDTO
+	// courseDTO, MentoReputationDTO mentoReputationDTO) {
+	// ModelAndView mv = new ModelAndView();
+	// /**
+	// * 누적 확인
+	// * */
+	// int MenteeCount = memberService.selectCountMentee(menteeDTO);
+	// int MentoCount = memberService.selectCountMento(mentoDTO);
+	// int CourseCount = memberService.selectCountCourse(courseDTO);
+	// int RepCount = memberService.selectCountReputation(mentoReputationDTO);
+	// List<MentoReputationDTO> mentoRepList = courseService.selectReputation();
+	// mv.addObject("MenteeCount", MenteeCount);
+	// mv.addObject("MentoCount", MentoCount);
+	// mv.addObject("CourseCount", CourseCount);
+	// mv.addObject("RepCount",RepCount);
+	// mv.addObject("mentoRepList", mentoRepList);
+	// mv.setViewName("main/mainpage/index");
+	// return mv;
+	// }
+
 	@RequestMapping("/index")
 	public String gohome() {
 		return "main/mainpage/index";
 	}
-	
+
 	@RequestMapping("/loginForm")
 	public String login() {
 		return "common/member/loginForm";
 	}
-	
+
 	@RequestMapping("/loginFind")
 	public String loginFind() {
 		return "common/member/loginFind";
 	}
-	
-	//jian
-	
+
+	// jian
+
 	@RequestMapping("/join")
 	public String join() {
 
 		return "common/member/join";
 	}
-	
+
 	@RequestMapping("/joinMentee")
 	public String joinMentee() {
 
 		return "common/member/joinMentee";
 	}
-	
+
 	@RequestMapping("/joinMento")
 	public String joinMento() {
 
 		return "common/member/joinMento";
 	}
-	
+
 	@RequestMapping("/course")
 	public String course() {
 
@@ -102,7 +101,5 @@ public class HomeController {
 
 		return "common/courese/courseDetail";
 	}
-	
-	
 
 }
